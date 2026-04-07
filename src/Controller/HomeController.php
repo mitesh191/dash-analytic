facf0d775178074bbbac628254153f0a7df47d40
Developer <devmp5353@gmail.com>
2026-04-07 23:17:18 +0530

    update routing

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,14 +7,6 @@ final class HomeController extends AbstractController
     #[Route("/", name: "app_home")]
     public function index(): Response
     {
-        $products = [
-            ["name" => "T-Shirt", "price" => 499],
-            ["name" => "Shoes", "price" => 1499],
-            ["name" => "Cap", "price" => 299],
-        ];
-        return $this->render("home/index.html.twig", [
-            "title" => "Symfony Demo Project",
-            "products" => $products,
-        ]);
+        return $this->redirectToRoute('dashboard_index');
     }
 }
